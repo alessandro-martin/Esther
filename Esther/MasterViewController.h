c887750b1ec92f468a66d0fc61f85b9e67bee187
Alessandro Martin <martin.acg@gmail.com>
2014-11-16 18:52:47 +0100

    Initial Commit

---
+++ Esther/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Esther
+//
+//  Created by Alessandro on 16/11/14.
+//  Copyright (c) 2014 Alessandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
